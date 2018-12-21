c04789838a3b31956fb43ea16490a5529bc9ecd2
adam cox <mr.adamcox@gmail.com>
2018-12-21 11:48:40 -0600

    change tabs to spaces
    Signed-off-by: adam cox <mr.adamcox@gmail.com>

--- eamena/eamena/views/api.py
+++ eamena/eamena/views/api.py
@@ -30,93 +30,93 @@ from django.forms.models import model_to_dict
 import numpy as np
 
 def create_info_resource(json):
-	info_res = {
-		'id': json['id'],
-		'child_entities': {
-			'URL.E51': json['url'],
-			'SPATIAL_COORDINATES_GEOMETRY.E47': '',
-			'DATE_OF_ACQUISITION.E50': '',
-			'DESCRIPTION.E62': '',
-			'IMAGERY_CREATOR_APPELLATION.E82': '',
-		},
-		'related_to': json['related_to']
-	}
-	resource = Resource({
-		'entitytypeid': 'INFORMATION_RESOURCE.E73',
-		'entityid': info_res['id']
-	})
-	schema = Entity.get_mapping_schema(resource.entitytypeid)
-	for key, value in info_res['child_entities'].iteritems():
-		entity = Entity()
-		if key in schema:
-			entity.create_from_mapping(resource.entitytypeid, schema[newsubentity['entitytypeid']]['steps'], newsubentity['entitytypeid'], newsubentity['value'], '')
-	resource.save()
-	
-	
-	#Insert relations
-	se = SearchEngineFactory().create()
-	relationship_type = '4f495e34-987a-44bb-ad24-183310f38d82' #this is the uuid of the concept value for the only type of relationship allowed between a HR and an Information resource
-	for related_res in info_res['related_to']:
-		relationship = resource.create_resource_relationship(related_res, relationship_type_id=relationship_type)
-		se.index_data(index='resource_relations', doc_type='all', body=model_to_dict(relationship), idfield='resourcexid')
+    info_res = {
+        'id': json['id'],
+        'child_entities': {
+            'URL.E51': json['url'],
+            'SPATIAL_COORDINATES_GEOMETRY.E47': '',
+            'DATE_OF_ACQUISITION.E50': '',
+            'DESCRIPTION.E62': '',
+            'IMAGERY_CREATOR_APPELLATION.E82': '',
+        },
+        'related_to': json['related_to']
+    }
+    resource = Resource({
+        'entitytypeid': 'INFORMATION_RESOURCE.E73',
+        'entityid': info_res['id']
+    })
+    schema = Entity.get_mapping_schema(resource.entitytypeid)
+    for key, value in info_res['child_entities'].iteritems():
+        entity = Entity()
+        if key in schema:
+            entity.create_from_mapping(resource.entitytypeid, schema[newsubentity['entitytypeid']]['steps'], newsubentity['entitytypeid'], newsubentity['value'], '')
+    resource.save()
+    
+    
+    #Insert relations
+    se = SearchEngineFactory().create()
+    relationship_type = '4f495e34-987a-44bb-ad24-183310f38d82' #this is the uuid of the concept value for the only type of relationship allowed between a HR and an Information resource
+    for related_res in info_res['related_to']:
+        relationship = resource.create_resource_relationship(related_res, relationship_type_id=relationship_type)
+        se.index_data(index='resource_relations', doc_type='all', body=model_to_dict(relationship), idfield='resourcexid')
 
 @csrf_exempt
 def create_resources(request):
-	if request.method == 'POST':
-		json_resources = JSONDeserializer().deserialize(request.body)
-		for json_resource in json_resources:
-			create_info_resource(json_resource)
-				
+    if request.method == 'POST':
+        json_resources = JSONDeserializer().deserialize(request.body)
+        for json_resource in json_resources:
+            create_info_resource(json_resource)
+                
 
-# 	return JSONResponse({'success': True})
+#     return JSONResponse({'success': True})
 
 @csrf_exempt
 def return_resources(request):
-	'''Endpoint to return nearest 1000 EAMENA resources given a GeoJSON bounding box'''
-	json_collection = []
-	if request.method == 'POST':
-		geojson = JSONDeserializer().deserialize(request.body)
-		se = SearchEngineFactory().create()
-		query= Query(se)
-		boolfilter = Bool()
-		geoshape = GeoShape(field='geometries.value', type=geojson['type'], coordinates=geojson['coordinates'] )
-		nested = Nested(path='geometries', query=geoshape)
-		boolfilter.must(nested)
-		query.add_query(boolfilter)
-		results = query.search(index='entity', doc_type='',start=0, limit = 1000)
-		for hit in results['hits']['hits']:
-			json_res = {
-				'resource_type': '',
-				'resource_id': '',
-				'resource_name' : '',
-				'centroid': ''
-                	}
-			json_res['resource_type'] = hit['_type']
-			json_res['resource_id'] = hit['_id']
-			json_res['resource_name'] = hit['_source']['primaryname']
-			json_res['centroid'] = generate_centroids(hit['_source']['geometries'])
-			json_collection.append(json_res)
-	return JSONResponse(json_collection)
-		
+    '''Endpoint to return nearest 1000 EAMENA resources given a GeoJSON bounding box'''
+    json_collection = []
+    if request.method == 'POST':
+        geojson = JSONDeserializer().deserialize(request.body)
+        se = SearchEngineFactory().create()
+        query= Query(se)
+        boolfilter = Bool()
+        geoshape = GeoShape(field='geometries.value', type=geojson['type'], coordinates=geojson['coordinates'] )
+        nested = Nested(path='geometries', query=geoshape)
+        boolfilter.must(nested)
+        query.add_query(boolfilter)
+        results = query.search(index='entity', doc_type='',start=0, limit = 1000)
+        for hit in results['hits']['hits']:
+            json_res = {
+                'resource_type': '',
+                'resource_id': '',
+                'resource_name' : '',
+                'centroid': ''
+                    }
+            json_res['resource_type'] = hit['_type']
+            json_res['resource_id'] = hit['_id']
+            json_res['resource_name'] = hit['_source']['primaryname']
+            json_res['centroid'] = generate_centroids(hit['_source']['geometries'])
+            json_collection.append(json_res)
+    return JSONResponse(json_collection)
+        
 def generate_centroids(geometries):
-	'''In some cases, resources will have multiple geometries. If that is the case, generating one centroid from the first available geometry won't be an accurate representation of the location of the overall resource. In that case, centre points are first generated for each geometry, then the centroid of the centre points is outputted. '''
+    '''In some cases, resources will have multiple geometries. If that is the case, generating one centroid from the first available geometry won't be an accurate representation of the location of the overall resource. In that case, centre points are first generated for each geometry, then the centroid of the centre points is outputted. '''
 
-	geom_list = []
-	if geometries:
-		if len(geometries) == 1:
-			geom = GEOSGeometry(JSONSerializer().serialize(geometries[0]['value']),srid =4326)
-			return JSONDeserializer().deserialize(geom.centroid.json)
-		else:			
-			for geometry in geometries:
-				geom = GEOSGeometry(JSONSerializer().serialize(geometry['value']),srid =4326)
-				centroid = JSONDeserializer().deserialize(geom.centroid.json)
-				geom_list.append(centroid['coordinates'])
-			arr = np.asarray(geom_list)
-			length = arr.shape[0]
-			sum_x = np.sum(arr[:, 0])
-			sum_y = np.sum(arr[:, 1])
-			wkt = 'POINT(%s, %s)' % (sum_x/length, sum_y/length)
-			return GEOSGeometry(wkt, srid = 4326).json
-			
-	else:
-		return None		
+    geom_list = []
+    if geometries:
+        if len(geometries) == 1:
+            geom = GEOSGeometry(JSONSerializer().serialize(geometries[0]['value']),srid =4326)
+            return JSONDeserializer().deserialize(geom.centroid.json)
+        else:            
+            for geometry in geometries:
+                geom = GEOSGeometry(JSONSerializer().serialize(geometry['value']),srid =4326)
+                centroid = JSONDeserializer().deserialize(geom.centroid.json)
+                geom_list.append(centroid['coordinates'])
+            arr = np.asarray(geom_list)
+            length = arr.shape[0]
+            sum_x = np.sum(arr[:, 0])
+            sum_y = np.sum(arr[:, 1])
+            wkt = 'POINT(%s, %s)' % (sum_x/length, sum_y/length)
+            return GEOSGeometry(wkt, srid = 4326).json
+            
+    else:
+        return None        
